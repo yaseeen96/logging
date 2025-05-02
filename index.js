28e58dcbed853c7a23b06a7fbab571c54e5d1df7
yaseeen96 <syedyaseen1539@gmail.com>
2025-05-03 00:36:33 +0530

    refactor code

--- index.js
+++ index.js
@@ -1,48 +1,7 @@
 import express from "express";
-import { createLogger, format, transports } from "winston";
-import winston from "winston";
+import logger from "./logger.js";
 
 const app = express();
-
-let alignColorsAndTime = winston.format.combine(
-  winston.format.colorize({
-    all: true,
-  }),
-  winston.format.label({
-    label: "LOGS",
-  }),
-  winston.format.timestamp({
-    format: "YY-MM-DD HH:MM:SS",
-  }),
-  winston.format.printf(
-    (info) =>
-      ` ${info.label}  ${info.timestamp}  ${info.level} : ${info.message}`
-  )
-);
-
-const logger = createLogger({
-  level: "info",
-  format: format.combine(
-    format.timestamp({
-      format: "YYYY-MM-DD HH:mm:ss",
-    }),
-    format.errors({ stack: true }),
-    format.splat(),
-    format.json()
-  ),
-  defaultMeta: { service: "TODO API" },
-  transports: [
-    //
-    // - Write to all logs with level `info` and below to `quick-start-combined.log`.
-    // - Write all logs error (and below) to `quick-start-error.log`.
-    //
-    new transports.File({ filename: "logs/error.log", level: "error" }),
-    new transports.File({ filename: "logs/debug.log", level: "debug" }),
-    new winston.transports.Console({
-      format: alignColorsAndTime,
-    }),
-  ],
-});
 
 app.get("/", (req, res) => {
   logger.info("Hello World");
