564e9bda1914a87681173da2df4cfaaa473a68a7
yaseeen96 <syedyaseen1539@gmail.com>
2025-05-03 02:46:08 +0530

    add test route to check the loggers, integrated by whatsapp

--- index.js
+++ index.js
@@ -2,12 +2,38 @@ import express from "express";
 import logger from "./logger.js";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  logger.info("Hello World");
-  logger.error("Error");
+  //   this is a test route add some real world example of a request with logging
 
-  res.send("Hello Worldss");
+  res.send("main server");
+});
+
+app.post("/test", (req, res) => {
+  //  this post request takes 2 numbers in body and sends a response in output json add logging on every step and also error step as well
+  // add error handling
+  try {
+    const { num1, num2 } = req.body;
+    if (!num1 || !num2) {
+      logger.error("Missing numbers");
+      return res.status(400).json({ error: "Missing numbers" });
+    }
+
+    const sum = num1 + num2;
+    logger.info(`Sum of ${num1} and ${num2} is ${sum}`);
+    res.json({ result: sum });
+  } catch (error) {
+    logger.error("Error in test route");
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/error", (req, res) => {
+  //   try {
+  //   winston.ooo();
+  // logger.info("Result:)0;
+  // res.send("Result: " + result);
 });
 
 app.listen(3000, () => {
